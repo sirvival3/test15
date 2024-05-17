c40308d3a9759667f27bdae30660da8d967b2898
sirvival3 <alexander@bogstav.dk>
2024-05-17 12:03:08 +0200

    Updated index

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,18 +1,18 @@
 import Link from "next/link";
-// import { getSession } from "@/actions/actions";
+import { getSession } from "../actions/actions";
 
 // import LogoutForm from "./logoutForm";
 
 export default async function Navbar() {
-//   const session = await getSession()
+  const session = await getSession()
 //   console.log(session)
 
   return (
     <nav>
         <Link href="/">Homepage</Link>
-        {/* {session.isLoggedIn && <Link href="/premium">Premium</Link>}
+        {session.isLoggedIn && <Link href="/premium">Premium</Link>}
         {session.isLoggedIn && <Link href="/profile">Profile</Link>}
-        {!session.isLoggedIn && <Link href="/login">Login</Link>}
+        {/* {!session.isLoggedIn && <Link href="/login">Login</Link>}
         {session.isLoggedIn && <LogoutForm/>} */}
     </nav>
   )
