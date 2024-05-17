4f982c8af87188ba500a041f858a9938bbbbdb1f
sirvival3 <alexander@bogstav.dk>
2024-05-17 12:11:56 +0200

    Updated index

--- app/premium/page.tsx
+++ app/premium/page.tsx
@@ -1,6 +1,35 @@
+import { getSession } from "@/actions/actions";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function Premium() {
-  return (
-    <div>Premium</div>
-  )
-}
+export default async function Premium() {
+  const session = await getSession()
+
+  if(!session.isLoggedIn){
+    redirect("/")
+  }
+
+  if(!session.isPro){
+    return(
+      <div className="notPremium">
+        <h1>Only premium users can see the content!</h1>
+        <Link href="/profile">
+          Go to the profile page to upgrade to premium
+        </Link>
+      </div>
+    )
+  }
+
+    return (
+      <main className="p-4">
+        <h1>Premium</h1>
+        <p>Welcome</p>
+        <ul>
+          <li>Apple</li>
+          <li>Orange</li>
+          <li>Peach</li>
+        </ul>
+      </main>
+    );
+  }
+  
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,28 @@
-export default function Profile() {
-  return (
-    <div>Profile</div>
-  )
-}
+import { changePremium, changeUSername, getSession } from "@/actions/actions";
+import { redirect } from "next/navigation";
+
+export default async function Profile() {
+  const session = await getSession()
+
+  if(!session.isLoggedIn){
+    redirect("/")
+  }
+
+    return (
+      <main className="p-4">
+        <h1>Profile</h1>
+        <p>
+          Welcome, <b>{session.username}</b>
+        </p>
+        <span>You are a <b>{session.isPro ? "Premium" : "Free"} user</b></span>
+        <form action={changePremium}>
+          <button>{session.isPro ? "Cancel" : "Buy"} Premium</button>     
+        </form>
+        <form action={changeUSername}>
+          <input type="text" name="username" required placeholder={session.username} />
+          <button>Update</button>
+        </form>
+      </main>
+    );
+  }
+  
