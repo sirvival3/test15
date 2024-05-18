e91823f5d2a2ec4ddbd1b033e6b85d56097959c6
sirvival3 <alexander@bogstav.dk>
2024-05-18 12:05:11 +0200

    Updated index

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { changePremium, changeUSername, getSession } from "../actions/actions";
+import { getSession } from "../actions/actions";
 import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
@@ -23,13 +23,6 @@ export default async function Dashboard() {
           Velkommen, <b>{session.username}</b>
         </p>
         <span>Du er <b>{session.role}</b></span>
-        {/* <form action={changePremium}>
-          <button>{session.isPro ? "Cancel" : "Buy"} Premium</button>     
-        </form> */}
-        {/* <form action={changeUSername}>
-          <input type="text" name="username" required placeholder={session.username} />
-          <button>Update</button>
-        </form> */}
       </main>
     );
   }
